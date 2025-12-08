03cef6c934f8d94036debcbebe4f453fd1920952
Julien Daudin <Julien.Daudin@eleves.ec-nantes.fr>
2025-12-08 16:10:50 +0100

    Réparation de l'erreur selon laquelle il n'y avait pas de return

---
+++ mavenproject1/src/main/java/natlien/mavenproject1/Dame.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package natlien.mavenproject1;
+
+/**
+ *
+ * @author julda
+ */
+public class Dame extends Pion{
+    private void deplacementdDiagonal(int direction,int nbCase){
+    };
+}
